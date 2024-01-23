06290be860de7104881e4e5c85ac21626be245b4
lv2dev <lv2dev@gmail.com>
2024-01-23 17:29:25 +0900

    first commit

---
+++ src/main/java/com/lv2dev/echonet/EchoNetBeApplication.java
@@ -0,0 +1,13 @@
+package com.lv2dev.echonet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoNetBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EchoNetBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lv2dev/echonet/EchoNetBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lv2dev.echonet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoNetBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
