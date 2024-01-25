e20a749f4225329163b16b57b443ea75962b403a
lv2dev <lv2dev@gmail.com>
2024-01-25 23:20:05 +0900

    MemberRepository 추가

---
+++ src/main/java/com/lv2dev/echonet/persistence/MemberRepository.java
@@ -0,0 +1,30 @@
+package com.lv2dev.echonet.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lv2dev.echonet.model.Member;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Integer> {
+    /**
+     * 이메일로 찾기
+     * */
+    Member findByEmail(String email);
+
+    /**
+     * existsByEmail
+     * */
+    boolean existsByEmail(String email);
+
+    /**
+     * 닉네임 중복 체크
+     * */
+    boolean existsByNickname(String nickname); // 닉네임 중복 체크를 위한 메소드
+
+    /**
+     * 아이디로 찾기
+     * */
+    Member findById(Long id);
+
+}
