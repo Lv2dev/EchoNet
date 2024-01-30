f8e4f8289473990eea89759d907534e8959dffbf
lv2dev <lv2dev@gmail.com>
2024-01-30 23:58:30 +0900

    SwaggerConfiguration을 Springdoc에 맞게 수정

--- src/main/java/com/lv2dev/echonet/config/SwaggerConfiguration.java
+++ src/main/java/com/lv2dev/echonet/config/SwaggerConfiguration.java
@@ -1,90 +1,33 @@
-package com.lv2dev.cloudguard.config;
+package com.lv2dev.echonet.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.SecurityReference;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
-import springfox.documentation.spring.web.plugins.Docket;
 
-import java.util.Arrays;
-import java.util.List;
-
-@OpenAPIDefinition(
-        info = @Info(title = "CloudGuard",
-                description = "각종 암호화 복호화 기능과 공유 기능을 갖춘 클라우드 스토리지",
-                version = "v1"))
 @Configuration
-@RequiredArgsConstructor
 public class SwaggerConfiguration {
 
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.lv2dev.cloudguard.controller"))
-                .paths(PathSelectors.regex("/public.*"))
-                .build()
-                .groupName("lv2dev")
-                .securityContexts(Arrays.asList(securityContext()))
-                .securitySchemes(Arrays.asList(apiKey()))
-                .apiInfo(apiInfo());
-    }
-
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("EchoNet API")
-                .description("설명은 고민중")
-                .version("1.0.0")
-                .build();
-    }
-
-    private ApiKey apiKey() {
-        return new ApiKey("JWT", "Authorization", "header Bearer");
-    }
-
-    private SecurityContext securityContext() {
-        return SecurityContext.builder()
-                .securityReferences(defaultAuth())
-                .build();
-    }
-
-
-    // Swagger 기본 인증 설정
-    private List<SecurityReference> defaultAuth() {
-        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-        authorizationScopes[0] = authorizationScope;
-        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
-    }
-
-    @Getter
-    @Setter
-    @Schema
-    static class Page {
-
-        @Schema
-        private Integer page;
-
-        @Schema(allowableValues = "range[0, 100]")
-        private Integer size;
-
-        @Schema
-        private List<String> sort;
-
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("JWT"))
+                .components(
+                        new Components()
+                                .addSecuritySchemes("JWT",
+                                        new SecurityScheme()
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                )
+                .info(new Info()
+                        .title("EchoNet Auth API")
+                        .description("에코넷 서비스의 인증 마이크로 서비스입니다.")
+                        .version("1.0.0"));
     }
 
 }
