d93cecdc99a70b9265174fe0b17486ec4bdd50f6
lv2dev <lv2dev@gmail.com>
2024-03-10 18:44:26 +0900

    로그인 기록 저장을 위한 LoginHistory Entity 추가

---
+++ src/main/java/com/lv2dev/echonet/model/LoginHistory.java
@@ -0,0 +1,33 @@
+package com.lv2dev.echonet.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "login_history")
+public class LoginHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private LocalDateTime loginTime;
+
+    private String ipAddress;
+
+    private String browserInfo;
+
+    private String deviceInfo;
+}
